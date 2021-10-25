7b7ae32836d2ca1945487286ce940aece827d7ac
Brian Donnoe <dunow33@hotmail.com>
2021-10-25 11:06:49 -0400

    first commit

---
+++ app/src/main/java/com/example/forecastapp/ui/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.forecastapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.forecastapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    //private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        //setupActionBarWithNavController(navController)
+        bottomNav.setupWithNavController(navController)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(null, navController)
+    }
+}
